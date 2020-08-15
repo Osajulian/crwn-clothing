9734848f61a8afba776e3481487c31550cf78b3f
Osa Julian <osajulian@hotmail.com>
2020-08-14 21:30:34 -0400

    created project with create react app and added homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Homepage from './homepage-component';
+import './App.css';
+
+function App() {
+  debugger
+  return (
+    <div>
+    <Homepage />
+    </div>
+  );
+}
+
+export default App;
